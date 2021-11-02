3e17fcc1d1fbeb613fc675f0768234c8f2437dd7
HarshitDubey007 <harshitdubey1996@gmail.com>
2021-11-02 15:47:21 +0530

    completed user authantication

---
+++ backend/controller/wallet.js
@@ -0,0 +1,19 @@
+const Wallet = require("../models/wallet");
+
+function createWallet(req, res){
+    try{
+        const Wallets = require("../models/wallets");
+        const { user_id, symbol } = req.body; 
+    } catch {
+
+    }
+}
+
+
+
+
+
+module.exports = {
+    createWallet
+ 
+};
