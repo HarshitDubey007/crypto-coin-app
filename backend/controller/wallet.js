82ee3d662c4b40a98570e63c810bbf77168bc204
HarshitDubey007 <harshitdubey1996@gmail.com>
2021-11-08 17:31:55 +0530

    wellet

--- backend/controller/wallet.js
+++ backend/controller/wallet.js
@@ -1,19 +1,28 @@
 const Wallet = require("../models/wallet");
 
-function createWallet(req, res){
-    try{
-        const Wallets = require("../models/wallets");
-        const { user_id, symbol } = req.body; 
-    } catch {
-
+function createWallet(req, res) {
+  try {
+    const Wallets = require("../models/wallets");
+    const { user_id, symbol } = req.body;
+    if (user_id && symbol) {
+      return res.status(200).json({ message: "This wellet already created" });
     }
+
+    const {
+      private_key,
+      wallet_address,
+      wallet_type,
+      balance,
+      v_balanace,
+      locked,
+      wallet_status,
+      contract_address,
+      date,
+      type
+    } = req.body;
+  } catch {}
 }
 
-
-
-
-
 module.exports = {
-    createWallet
- 
+  createWallet,
 };
